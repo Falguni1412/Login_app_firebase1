23a13fea2c650a7b5a9547a507d7a9c03f6d8063
Falguni1412 <falgunitimande72@gmail.com>
2024-11-16 01:11:57 +0530

    next

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function w3_open() {
+    document.getElementById("mySidebar").style.left = "0"; // Open sidebar by setting right to 0
+}
+
+function w3_close() {
+    document.getElementById("mySidebar").style.left = "-250px"; // Hide sidebar by setting right to -250px
+}
